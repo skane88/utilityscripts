5c9d6e2d3fae40c893a7d45559351a83c83a6e40
skane88 <sean.kane@outlook.com.au>
2019-01-30 07:11:21 +1000

    Changed from os to pathlib
    I like the pathlib API better...

--- Python/FileLister.py
+++ Python/FileLister.py
@@ -1,28 +1,42 @@
-import os
-
+from pathlib import Path
 
 def lister():
     base_path = input("Provide the base path: ")
 
-    true_dict = {"n": False, "no": False, "y": True, "yes": True}
-    file_path_bool = None
+    base_path = Path(base_path)
 
-    while file_path_bool is None:
-        file_path_bool = input("Do you want to include the full file path (Y or N)? ")
+    if base_path.is_dir():
 
-        file_path_bool = true_dict.get(file_path_bool.lower(), None)
+        true_dict = {"n": False, "no": False, "y": True, "yes": True}
+        file_path_bool = None
+        recursive_path = None
 
-    for path, folders, files in os.walk(base_path):
+        while recursive_path is None:
+            recursive_path = input("Do you want to include subfolders (Y or N)?")
 
-        for fi in files:
+            recursive_path = true_dict.get(recursive_path.lower(), None)
 
-            file_path = os.path.join(path, fi)
+        while file_path_bool is None:
+            file_path_bool = input("Do you want to include the full file path (Y or N)? ")
+
+            file_path_bool = true_dict.get(file_path_bool.lower(), None)
+
+        if recursive_path:
+            f_iterator = base_path.rglob('*')
+        else:
+            f_iterator = base_path.iterdir()
+
+        for f in f_iterator:
 
             if not file_path_bool:
-                print(fi)
+                print(f.name)
             else:
-                print(file_path)
-
+                print(f)
+    else:
+        print('The provided path is not a directory.')
+    
+    print()
+    input('Press any key to exit.')
 
 if __name__ == "__main__":
     lister()
