96e98b209343aed00123d1d61099826d0f4b6d3f
skane88 <sean.kane@outlook.com.au>
2019-01-29 14:44:32 +1000

    Minor change to true_dict to be a bit less restrictive.
    Also made the test test against str.lower() so that capitalisation is
    not important.

--- Python/FileLister.py
+++ Python/FileLister.py
@@ -4,13 +4,13 @@ import os
 def lister():
     base_path = input("Provide the base path: ")
 
-    true_dict = {"n": False, "N": False, "y": True, "Y": True}
+    true_dict = {"n": False, "no": False, "y": True, "yes": True}
     file_path_bool = None
 
     while file_path_bool is None:
         file_path_bool = input("Do you want to include the full file path (Y or N)? ")
 
-        file_path_bool = true_dict.get(file_path_bool, None)
+        file_path_bool = true_dict.get(file_path_bool.lower(), None)
 
     for path, folders, files in os.walk(base_path):
 
