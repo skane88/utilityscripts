0d3af20ae60d9f70c6d40552a8bc3c790bced04d
skane88 <sean.kane@outlook.com.au>
2019-01-30 07:12:30 +1000

    Minor format changes.
    Ran Black.

--- Python/FileLister.py
+++ Python/FileLister.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def lister():
     base_path = input("Provide the base path: ")
 
@@ -12,17 +13,19 @@ def lister():
         recursive_path = None
 
         while recursive_path is None:
-            recursive_path = input("Do you want to include subfolders (Y or N)?")
+            recursive_path = input("Do you want to include subfolders (Y or N)? ")
 
             recursive_path = true_dict.get(recursive_path.lower(), None)
 
         while file_path_bool is None:
-            file_path_bool = input("Do you want to include the full file path (Y or N)? ")
+            file_path_bool = input(
+                "Do you want to include the full file path (Y or N)? "
+            )
 
             file_path_bool = true_dict.get(file_path_bool.lower(), None)
 
         if recursive_path:
-            f_iterator = base_path.rglob('*')
+            f_iterator = base_path.rglob("*")
         else:
             f_iterator = base_path.iterdir()
 
@@ -33,10 +36,11 @@ def lister():
             else:
                 print(f)
     else:
-        print('The provided path is not a directory.')
-    
+        print("The provided path is not a directory.")
+
     print()
-    input('Press any key to exit.')
+    input("Press any key to exit.")
+
 
 if __name__ == "__main__":
     lister()
