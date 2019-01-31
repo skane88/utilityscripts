dee085aee975a148afb489a420331d2b175fc4fd
skane88 <sean.kane@outlook.com.au>
2019-01-31 10:32:18 +1000

    General updates.

--- Python/FileLister.py
+++ Python/FileLister.py
@@ -9,32 +9,52 @@ def lister():
     if base_path.is_dir():
 
         true_dict = {"n": False, "no": False, "y": True, "yes": True}
-        file_path_bool = None
-        recursive_path = None
+        file_path_bool: bool = None
+        recursive_path: bool = None
+        incl_extension: bool = None
 
         while recursive_path is None:
-            recursive_path = input("Do you want to include subfolders (Y or N)? ")
+            r = input("Do you want to include subfolders (Y or N)? ")
 
-            recursive_path = true_dict.get(recursive_path.lower(), None)
+            recursive_path = true_dict.get(r.lower(), None)
 
         while file_path_bool is None:
-            file_path_bool = input(
+            fp = input(
                 "Do you want to include the full file path (Y or N)? "
             )
 
-            file_path_bool = true_dict.get(file_path_bool.lower(), None)
+            file_path_bool = true_dict.get(fp.lower(), None)
+
+        while incl_extension is None:
+            ie = input("Do you want to include the extension (Y or N)? ")
+
+            incl_extension = true_dict.get(ie.lower(), None)
+
+        # now we've got input, now do the actual finding of files
 
         if recursive_path:
+            # if subfolders are required, use rglob
             f_iterator = base_path.rglob("*")
         else:
+            # if only the local folder, just use iterdir
             f_iterator = base_path.iterdir()
 
+        print()
+
         for f in f_iterator:
+            
+            f: Path()
+
+            text = str(f)
 
             if not file_path_bool:
-                print(f.name)
-            else:
-                print(f)
+                text = f.name
+
+            if not incl_extension:
+                text = text.replace(f.suffix, '')
+
+            print(text)
+            
     else:
         print("The provided path is not a directory.")
 
