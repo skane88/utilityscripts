f2ae0a992a11e5e816756d01bd8f5137cbbf1867
skane88 <sean.kane@outlook.com.au>
2019-02-07 11:56:18 +1000

    Added maximum number of iterations.

--- Python/solvers.py
+++ Python/solvers.py
@@ -26,6 +26,7 @@ def bisection(
     x_low: float = float_info.min,
     x_high: float = float_info.max,
     tol: float = 1e-10,
+    max_its=None,
 ):
     """
     Implements the bi-section method.
@@ -37,11 +38,21 @@ def bisection(
     :param x_low: The lower bounds of the range to check.
     :param x_high: The upper bounds of the range to check.
     :param tol: The solution tolerance.
+    :param max_its: A maximum number of iterations to perform. If convergence is not
+        achieved within tol when max_its is reached, an error is raised.
+
+        If ``None``, the solver will continue until convergence is reached (potentially
+        infinitely, although it is likely that your computer's numerical precision will
+        result in convergence before an infinite number of iterations is reached)
     :returns: Returns a tuple: (root, no. of iterations)
     """
 
     if x_high == x_low:
         raise ValueError("Expected guesses to be different.")
+
+    if max_its is not None:
+        if max_its <= 1:
+            raise ValueError("Maximum no. of iterations should be > 1")
 
     i = 0
 
@@ -62,6 +73,13 @@ def bisection(
             x_high = x_mid
 
         i += 1
+
+        if max_its is not None:
+            if i > max_its:
+                raise ValueError(
+                    f"Exceeded maximum number of iterations. "
+                    + f"Current root approximation is {x_mid}."
+                )
 
     return (x_low + x_high) / 2, i
 
