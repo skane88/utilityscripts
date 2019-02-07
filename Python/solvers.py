f188f1df28366ac93ad2b624630ab4f9ab4aae46
skane88 <sean.kane@outlook.com.au>
2019-02-07 11:25:29 +1000

    Added file that implements a bi-section solver.

---
+++ Python/solvers.py
@@ -0,0 +1,66 @@
+"""
+Implements the bisection method.
+"""
+
+from sys import float_info
+
+
+def test_func(x):
+
+    return x * 2 / 3 - 1
+
+
+def sign(num):
+    """
+    Check the sign of a number. Returns -1 if less than 0, otherwise returns 1.
+    """
+
+    if num < 0:
+        return -1
+    else:
+        return 1
+
+
+def bisection(
+    func,
+    guess_low: float = float_info.min,
+    guess_high: float = float_info.max,
+    tol: float = 1e-10,
+):
+    """
+    Implements the bi-section method.
+    """
+
+    if guess_high == guess_low:
+        raise ValueError("Expected guesses to be different.")
+
+    x_low = guess_low
+    x_high = guess_high
+    i=0
+
+    while abs(x_high - x_low) > tol and x_high != x_low:
+
+        y_low = func(x_low)
+        y_high = func(x_high)
+
+        if sign(y_low) == sign(y_high):
+            raise ValueError("Expected the guesses to bracket the root")
+
+        x_mid = (x_low + x_high) / 2
+        y_mid = func(x_mid)
+
+        if sign(y_low) == sign(y_mid):
+            x_low = x_mid
+        else:
+            x_high = x_mid
+
+        i += 1
+
+    return (x_low + x_high) / 2, i
+
+
+if __name__ == "__main__":
+
+    print("Test")
+    print(f"Solution is: {bisection(test_func)[0]} in {bisection(test_func)[1]} iterations")
+    input("Press any key to exit")
