38858c1cc1a4b181f245f144840356112ce88699
skane88 <sean.kane@outlook.com.au>
2019-02-09 13:32:28 +1000

    Updated with latest changes from BeamDesign library.

--- Python/solvers.py
+++ Python/solvers.py
@@ -1,45 +1,19 @@
 """
-Implements the bisection method.
+This file contains a number of solvers for general use.
 """
 
 from sys import float_info
-from math import sin
-
-
-def test_func(x):
-
-    return x * 2 / 3 - 1
-
-
-def test_func2(x):
-
-    return x ** 4 - x - 10
-
-
-def test_func3(x):
-
-    return x - sin(x) - (1 / 2)
-
-
-def sign(num):
-    """
-    Check the sign of a number. Returns -1 if less than 0, otherwise returns 1.
-    """
-
-    if num < 0:
-        return -1
-    else:
-        return 1
+from math import isclose, isnan, isinf
 
 
 def bisection(
     func,
-    x_low: float = float_info.min,
-    x_high: float = float_info.max,
-    tol: float = 1e-10,
-    max_its: int=None,
-    fail_on_max_its: bool=True,
     *args,
+    x_low: float = -float_info.max,
+    x_high: float = float_info.max,
+    tol: float = 1e-9,
+    max_its: int = 20000,
+    fail_on_max_its: bool = True,
     **kwargs,
 ):
     """
@@ -51,7 +25,10 @@ def bisection(
     :param func: A function with a single input parameter ('x') to be solved for 0.
     :param x_low: The lower bounds of the range to check.
     :param x_high: The upper bounds of the range to check.
-    :param tol: The solution tolerance.
+    :param tol: The solution tolerance. A default value of 1e-9 is provided. Note that
+        smaller values may cause trouble with convergence, possibly due to floating
+        point issues.
+        NOTE: this is an absolute, not a relative, tolerance.
     :param max_its: A maximum number of iterations to perform. If convergence is not
         achieved within tol when max_its is reached, an error is raised.
 
@@ -64,8 +41,21 @@ def bisection(
     :returns: Returns a tuple: (root, no. of iterations)
     """
 
-    if x_high == x_low:
-        raise ValueError("Expected guesses to be different.")
+    if isinf(x_low) or isinf(x_high):
+        raise ValueError(
+            f"Guesses should not be inf: " + f"x_low={x_low}, x_high={x_high}"
+        )
+
+    if isnan(x_low) or isnan(x_high):
+        raise ValueError(
+            f"Guesses should not be nan: " + f"x_low={x_low}, x_high={x_high}"
+        )
+
+    if isclose(x_high, x_low, abs_tol=tol):
+        raise ValueError(
+            f"Expected guesses to be different. Current guesses: "
+            + f"x_low={x_low}, x_high={x_high}"
+        )
 
     if max_its is not None:
         if max_its <= 1:
@@ -73,20 +63,46 @@ def bisection(
 
     i = 0
 
-    while abs(x_high - x_low) > tol and x_high != x_low:
+    while abs(x_high - x_low) > tol and not isclose(x_high, x_low, abs_tol=tol):
 
         i += 1
 
         y_low = func(x_low, *args, **kwargs)
         y_high = func(x_high, *args, **kwargs)
 
-        if sign(y_low) == sign(y_high):
-            raise ValueError("Expected the guesses to bracket the root")
+        # if y_low or y_high luck out and end on 0.0, we can report them as the roots.
+        if isclose(y_low, 0.0, abs_tol=tol):
+            return x_low, i
+        if isclose(y_high, 0.0, abs_tol=tol):
+            return x_high, i
+
+        if isinf(y_low) or isinf(y_high):
+            raise ValueError(
+                f"Either x_low or x_high result in infinity. No valid solution can be "
+                + f"found. Current guesses: "
+                + f"(x_low, y_low)=({x_low},{y_low}), "
+                + f"(x_high, y_high)=({x_high}, {y_high})"
+            )
+
+        if isnan(y_low) or isnan(y_high):
+            raise ValueError(
+                f"Either x_low or x_high result in nan. No valid solution can be "
+                + f"found. Current guesses: "
+                + f"(x_low, y_low)=({x_low},{y_low}), "
+                + f"(x_high, y_high)=({x_high}, {y_high})"
+            )
+
+        if y_low * y_high > 0.0:
+            raise ValueError(
+                f"Expected the guesses to bracket the root. Current guesses: "
+                + f"(x_low, y_low)=({x_low},{y_low}), "
+                + f"(x_high, y_high)=({x_high}, {y_high})"
+            )
 
         x_mid = (x_low + x_high) / 2
         y_mid = func(x_mid, *args, **kwargs)
 
-        if sign(y_low) == sign(y_mid):
+        if y_low * y_mid > 0.0:
             x_low = x_mid
         else:
             x_high = x_mid
@@ -106,12 +122,12 @@ def bisection(
 
 def secant(
     func,
+    *args,
     x_low: float,
     x_high: float,
-    tol: float = 1e-10,
-    max_its: int = None,
-    fallback: bool = True,
-    *args,
+    tol: float = 1e-9,
+    max_its: int = 20000,
+    fallback: bool = False,
     **kwargs,
 ):
     """
@@ -133,7 +149,17 @@ def secant(
     :param func: A function with a single input parameter ('x') to be solved for 0.
     :param x_low: The first initial guess.
     :param x_high: The second initial guess.
-    :param tol: The solution tolerance.
+        Note: if there is a possibility that the root is very large or very small,
+        floating point arithmetic may result in guesses that are close together (e.g.
+        within say 1.0 of each other) giving identical solutions for the root,
+        resulting in a divide by zero error.
+
+        For example, solving (x + 9007199254740992) with guesses of 0.0 and 1.0 results
+        in both func(x_low) and func(x_high) giving 9007199254740992.
+    :param tol: The solution tolerance. A default value of 1e-9 is provided. Note that
+        smaller values may cause trouble with convergence, possibly due to floating
+        point issues.
+        NOTE: this is an absolute, not a relative, tolerance.
     :param max_its: A maximum number of iterations to perform. If convergence is not
         achieved within tol when max_its is reached, an error is raised.
 
@@ -151,17 +177,62 @@ def secant(
         bisection_used is True if the method falls back to the bisection method.
     """
 
+    if isinf(x_low) or isinf(x_high):
+        raise ValueError(
+            f"Guesses should not be inf: " + f"x_low={x_low}, x_high={x_high}"
+        )
+
+    if isnan(x_low) or isnan(x_high):
+        raise ValueError(
+            f"Guesses should not be nan: " + f"x_low={x_low}, x_high={x_high}"
+        )
+
+    if isclose(x_high, x_low, abs_tol=tol):
+        raise ValueError(
+            f"Expected guesses to be different. Current guesses: "
+            + f"x_low={x_low}, x_high={x_high}"
+        )
+
     i = 0
     x_1 = x_low
     x_2 = x_high
 
-    while abs(x_2 - x_1) > tol and x_1 != x_2:
+    while abs(x_2 - x_1) > tol and not isclose(x_1, x_2, abs_tol=tol):
 
         i += 1
 
-        x_3 = (x_1 * func(x_2, *args, **kwargs) - x_2 * func(x_1, *args, **kwargs)) / (
-            func(x_2, *args, **kwargs) - func(x_1, *args, **kwargs)
-        )
+        a = x_1 * func(x_2, *args, **kwargs)
+        b = x_2 * func(x_1, *args, **kwargs)
+        c = func(x_2, *args, **kwargs)
+        d = func(x_1, *args, **kwargs)
+
+        if isinf(a) or isinf(b) or isinf(c) or isinf(d):
+            raise ValueError(
+                f"The solution to the functions results in an infinite "
+                + f"value. Recommend different initial guesses. Equation being solved "
+                + f"is: (a - b) / (c - d). "
+                + f"a = x_low x func(x_high) = {a}, "
+                + f"b = x_high x func(x_low) = {b}, "
+                + f"c = func(x_high) = {c}, "
+                + f"d = func(x_low) = {d}."
+            )
+
+        if isclose(c, d, abs_tol=tol):
+            raise ValueError(
+                f"Both guesses result in the same solution to the function, probably due"
+                + f" to floating point arithmetic errors. This will result in a divide "
+                + f"by zero error. Current guesses x_low = {x_1}, x_high = {x_2}. "
+                + f"Denominator in solution is ({c} - {d} = {c - d}). Consider "
+                + f"different initial guesses"
+            )
+
+        x_3 = (a - b) / (c - d)
+
+        if isnan(x_3) or isinf(x_3):
+            raise ValueError(
+                f"Guessed solution is inf or nan. Current guesses are: "
+                + f"x_low = {x_1}, x_high={x_2}, guessed solution is {x_3}"
+            )
 
         x_1 = x_2
         x_2 = x_3
@@ -181,58 +252,3 @@ def secant(
                 )
 
     return x_3, i, False
-
-
-if __name__ == "__main__":
-
-    print("Testing function 1:")
-
-    x, i = bisection(test_func)
-    print(f"Solution by method of bisection is: {x}, in {i} iterations")
-
-    x, i = bisection(test_func, max_its=1030, fail_on_max_its=False)
-    print(f"Solution by method of bisection is: {x}, in {i} iterations")
-
-    x, i, b = secant(test_func, x_low=float_info.min / 10, x_high=float_info.max / 10)
-    print(
-        f"Solution by secant method is: {x}, in {i} iterations. "
-        + f"Fallback to bisection? {b}"
-    )
-
-    print()
-    print("Testing function 1 with different guesses:")
-
-    x, i = bisection(test_func, x_low=-10, x_high=10)
-    print(f"Solution by method of bisection is: {x}, in {i} iterations")
-
-    x, i, b = secant(test_func, x_low=-10, x_high=10)
-    print(
-        f"Solution by secant method is: {x}, in {i} iterations. "
-        + f"Fallback to bisection? {b}"
-    )
-
-    print()
-    print("Testing function 2:")
-
-    x, i = bisection(test_func2, x_low=1, x_high=2)
-    print(f"Solution by method of bisection is: {x}, in {i} iterations")
-
-    x, i, b = secant(test_func2, x_low=1, x_high=2)
-    print(
-        f"Solution by secant method is: {x}, in {i} iterations. "
-        + f"Fallback to bisection? {b}"
-    )
-
-    print()
-    print("Testing function 3:")
-
-    x, i = bisection(test_func3, x_low=1, x_high=2)
-    print(f"Solution by method of bisection is: {x}, in {i} iterations")
-
-    x, i, b = secant(test_func3, x_low=1, x_high=2)
-    print(
-        f"Solution by secant method is: {x}, in {i} iterations. "
-        + f"Fallback to bisection? {b}"
-    )
-
-    input("Press any key to exit")
