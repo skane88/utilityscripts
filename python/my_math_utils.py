7a10eab49343ef6620c0d3d88c052413afa963b8
Sean Kane <sean.kane@outlook.com.au>
2021-02-19 08:08:10 +1000

    add a round_significant method (to be used for some testing later)

--- python/my_math_utils.py
+++ python/my_math_utils.py
@@ -12,6 +12,39 @@ def round_nearest(x, a):
     return round(round(x / a) * a, -int(math.floor(math.log10(a))))
 
 
+def round_significant(x, s: int = 3):
+    """
+    Round a number to a specified significant figure.
+
+    Based on an answer at
+
+    https://stackoverflow.com/a/9557559
+
+    :param x: The number to round.
+    :param s: The significance to round to.
+    """
+
+    if x == 0:
+        # bail out early on 0.
+        return 0
+
+    # can't take a log of a -ve number, so have to abs x
+    xsign = x / abs(x)
+    x = abs(x)
+
+    # get the exponent in powers of 10s
+    max10exp = math.floor(math.log10(x))
+
+    # figure out the power we need to get the correct no. of significant figures
+    sig10pow = 10 ** (max10exp - s + 1)
+
+    # raie the number to have s digits past the decimal point
+    floated = x * 1.0 / sig10pow
+
+    # drop it back down to the original power of 10 & return
+    return round(floated) * sig10pow * xsign
+
+
 if __name__ == "__main__":
 
     round_pairs = [(11.1212341234, 0.25), (-12.34, 0.5), (0.0623413, 0.1)]
@@ -25,5 +58,5 @@ if __name__ == "__main__":
 
     print()
     print("Done")
-    print("="*10)
+    print("=" * 10)
     print()
