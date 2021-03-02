7b7643c5c7bf8fa6dd0926f6a71cc6bcea09b161
skane88 <sean.kane@outlook.com.au>
2021-03-02 14:33:18 +1000

    add test for M_d calculation

--- tests/test_as1170_2.py
+++ tests/test_as1170_2.py
@@ -3,11 +3,12 @@ Contains some tests for the AS1170.2 module.
 """
 
 from pathlib import Path
+from math import isclose
 
 import toml
 import pytest
 
-from as1170_2 import V_R
+from as1170_2 import V_R, M_d
 
 FILE_PATH = Path(__file__)
 TEST_DATA_PATH = FILE_PATH.parent / Path("test_as1170_2.toml")
@@ -61,3 +62,41 @@ def test_V_R(input_vals, expected):
     V_R_calc = round(V_R_calc)  # round because the data from AS1170 is rounded
 
     assert V_R_calc == expected
+
+
+def build_direction_pairs():
+
+    test_data = TEST_DATA["wind_direction_factor"]
+
+    pairs = []
+
+    for region, v in test_data.items():
+
+        for direction, M_d in v.items():
+
+            if direction.lower() != "any":
+                direction = float(direction)
+
+            M_d = tuple(M_d)
+
+            pairs.append([(region, direction), M_d])
+
+            if isinstance(direction, str):
+                continue
+
+            pairs.append([(region, direction + 360), M_d])
+            pairs.append([(region, direction - 360), M_d])
+
+    return pairs
+
+
+@pytest.mark.parametrize("input_vals, expected", build_direction_pairs())
+def test_M_d(input_vals, expected):
+
+    wind_region = input_vals[0]
+    direction = input_vals[1]
+
+    M_d_calc = M_d(wind_region=wind_region, direction=direction)
+
+    assert isclose(M_d_calc[0], expected[0])
+    assert isclose(M_d_calc[1], expected[1])
