5058d206834a762190238189fff2e907af1b36d0
skane88 <sean.kane@outlook.com.au>
2021-06-21 15:07:09 +1000

    add a function to integrate numpy arrays

---
+++ python/np_integrate.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def integrate(x: np.ndarray, y: np.ndarray):
+    """
+    Integrate 2x numpy arrays.
+
+    :param x: The x points of the function to integrate.
+    :param y: The y points of the function to integrate.
+    """
+
+    x0 = x[:-1]
+    x1 = x[1:]
+    y0 = y[:-1]
+    y1 = y[1:]
+
+    delta = x1 - x0
+
+    return np.sum(((y0 + y1) / 2) * delta)
