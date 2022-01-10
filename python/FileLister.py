8327526712d6fa9610845415113dcc2ddfc16dfe
skane88 <sean.kane@outlook.com.au>
2022-01-10 14:06:08 +1000

    various updates to flake8 and pylint linting

--- python/FileLister.py
+++ python/FileLister.py
@@ -1,6 +1,6 @@
 """
-Lists all the folders / files in a given directory, and also has the ability to save
-the list to a text file.
+Lists all the folders / files in a given directory, and also has the ability
+to save the list to a text file.
 """
 
 from pathlib import Path
@@ -26,37 +26,41 @@ def lister():
         sort_file: bool = None
 
         while recursive is None:
-            r = input("Do you want to search subfolders (Y or N)? ")
+            recursive = input("Do you want to search subfolders (Y or N)? ")
 
-            recursive = true_dict.get(r.lower())
+            recursive = true_dict.get(recursive.lower())
 
         while report_folders is None:
-            r = input("Do you want to report folders (Y or N)? ")
+            report_folders = input("Do you want to report folders (Y or N)? ")
 
-            report_folders = true_dict.get(r.lower())
+            report_folders = true_dict.get(report_folders.lower())
 
         while incl_full_path is None:
-            fp = input("Do you want to include the full file path (Y or N)? ")
+            incl_full_path = input(
+                "Do you want to include the full file path (Y or N)? "
+            )
 
-            incl_full_path = true_dict.get(fp.lower())
+            incl_full_path = true_dict.get(incl_full_path.lower())
 
         if not incl_full_path:
             while incl_relative_path is None:
-                rp = input("Do you want to include the relative path (Y or N)?")
+                incl_relative_path = input(
+                    "Do you want to include the relative path (Y or N)?"
+                )
 
-                incl_relative_path = true_dict.get(rp.lower())
+                incl_relative_path = true_dict.get(incl_relative_path.lower())
         else:
             incl_relative_path = False
 
         while incl_extension is None:
-            ie = input("Do you want to include the extension (Y or N)? ")
+            incl_extension = input("Do you want to include the extension (Y or N)? ")
 
-            incl_extension = true_dict.get(ie.lower())
+            incl_extension = true_dict.get(incl_extension.lower())
 
         while filter_type is None:
-            ft = input("Do you want to filter by file type (Y or N)? ")
+            filter_type = input("Do you want to filter by file type (Y or N)? ")
 
-            filter_type = true_dict.get(ft.lower())
+            filter_type = true_dict.get(filter_type.lower())
 
         if filter_type:
             filter_val = input(
@@ -64,19 +68,21 @@ def lister():
             )
 
         while save_to_file is None:
-            stf = input("Do you want to save to a file (Y or N)?")
+            save_to_file = input("Do you want to save to a file (Y or N)?")
 
-            save_to_file = true_dict.get(stf.lower())
+            save_to_file = true_dict.get(save_to_file.lower())
 
         while all_lower_case is None:
-            alc = input("Do you want to convert file names to lower case (Y or N)?")
+            all_lower_case = input(
+                "Do you want to convert file names to lower case (Y or N)?"
+            )
 
-            all_lower_case = true_dict.get(alc.lower())
+            all_lower_case = true_dict.get(all_lower_case.lower())
 
         while sort_file is None:
-            sf = input("Do you want to sort the file names (Y or N)?")
+            sort_file = input("Do you want to sort the file names (Y or N)?")
 
-            sort_file = true_dict.get(sf.lower())
+            sort_file = true_dict.get(sort_file.lower())
 
         # now we've got input, now do the actual finding of files
 
@@ -146,8 +152,8 @@ def lister():
 
             with open(output_file, "w") as f:
 
-                for l in text_to_save:
-                    f.write(f"{l}\n")
+                for line in text_to_save:
+                    f.write(f"{line}\n")
 
     else:
         print("The provided path is not a directory.")
