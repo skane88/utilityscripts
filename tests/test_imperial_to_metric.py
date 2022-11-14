f0b848c370e8b061f9edf50f683b66d4a486c487
Sean Kane <skane88@users.noreply.github.com>
2022-11-14 21:30:46 +1000

    Add additional tests.

--- tests/test_imperial_to_metric.py
+++ tests/test_imperial_to_metric.py
@@ -10,7 +10,11 @@ from utilityscripts.imperial_to_metric import feet_inches_to_m
 
 
 @pytest.mark.parametrize(
-    "example,expected", [(r"23'", 7.0104), (r'12"', 0.3048), (r'11.0"', 0.3048), (r"12'" + r'9.5"', 3.8989)]
+    "example,expected", [(r"23'", 7.0104),
+    (r'12"', 0.3048),
+    (r'11.0"', 0.3048),
+    (r'11 1/2"', 0.2921),
+    (r"12'" + r'9.5"', 3.8989)]
 )
 def test_convert_to_metric(example, expected):
 
