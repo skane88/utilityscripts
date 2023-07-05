d3d16342a073f479f3b2ce28351e918ae6da86ec
Sean Kane <sean.kane@outlook.com.au>
2023-07-05 11:29:19 +1000

    add Boussinesq's equations and tests to geotech.py

--- tests/test_geotech.py
+++ tests/test_geotech.py
@@ -4,7 +4,15 @@ Contains tests for the geotech module.
 
 from math import isclose, radians
 
-from utilityscripts.geotech import q_ult
+import numpy as np
+import pytest
+
+from utilityscripts.geotech import (
+    boussinesq_patch_sigma_x,
+    boussinesq_patch_sigma_z,
+    boussinesq_point_sigma_z,
+    q_ult,
+)
 
 
 def test_q_ult():
@@ -33,3 +41,114 @@ def test_q_ult():
     )
 
     assert isclose(expected, actual, rel_tol=0.001)
+
+
+@pytest.mark.parametrize(
+    "Q, z, r, expected, tol",
+    [
+        (1000, 5, 0, 19.10, 0.0001),
+        (225, 0.001, 0, 1e8, 0.1),  # see J Bowles
+        (225, 0.6, 0, 298, 0.005),  # see J Bowles
+        (225, 1.2, 0, 74.5, 0.005),  # see J Bowles
+        (225, 3.0, 0, 11.9, 0.005),  # see J Bowles
+        (500, 2, 2, 10.5, 0.005),  # see J Bowles
+        (1000, 2, 1, 68.3, 0.005),  # see J Bowles
+        (1500, 5, 0, 29, 0.05),  # see Craig's soil mechanics
+    ],
+)
+def test_boussinesq_point_sigma_z(Q, z, r, expected, tol):
+    """
+    Test Boussinesq's method based on values calculated at:
+
+    https://testbook.com/civil-engineering/boussinesqs-equation-definition-and-hypothesis
+
+    and in J. Bowles 5ed.
+
+    """
+
+    actual = boussinesq_point_sigma_z(Q=Q, z=z, r=r)
+
+    assert isclose(expected, actual, rel_tol=tol)
+
+
+@pytest.mark.parametrize(
+    "Q, z, r, x_patch, y_patch, expected, tol",
+    [
+        (1000, 5, 0, 0.00001, 0.00001, 19.10, 0.0001),
+        (225, 0.001, 0, 0.00001, 0.00001, 1e8, 0.1),
+        (225, 0.6, 0, 0.00001, 0.00001, 298, 0.005),
+        (225, 1.2, 0, 0.00001, 0.00001, 74.5, 0.005),
+        (225, 3.0, 0, 0.00001, 0.00001, 11.9, 0.005),
+        (500, 2, 2, 0.00001, 0.00001, 10.5, 0.005),
+        (1000, 2, 1, 0.00001, 0.00001, 68.3, 0.005),
+        (1500, 5, 0, 2, 2, 27, 0.01),
+        (
+            300 * 6 * 3,
+            3,
+            3,
+            3,
+            6,
+            44,
+            0.005,
+        ),  # additional test from Craig's soil mechanics.
+        (
+            250 * 2 * 100,
+            3,
+            0,
+            2,
+            100,
+            99,
+            0.005,
+        ),  # additional test from Craig's soil mechanics
+    ],
+)
+@pytest.mark.filterwarnings("ignore:warning")
+def test_boussinesq_patch_sigma_z(Q, z, r, x_patch, y_patch, expected, tol):
+    """
+    Test the Boussinesq patch method against the point loads tested above,
+    using a very small patch area.
+    """
+
+    x_patch = x_patch
+    y_patch = y_patch
+
+    q = Q / (x_patch * y_patch)
+
+    actual = boussinesq_patch_sigma_z(
+        q=q, x=r, y=0, z=z, x_patch=x_patch, y_patch=y_patch, n_int_x=100, n_int_y=100
+    )
+
+    assert isclose(expected, actual, rel_tol=tol)
+
+
+@pytest.mark.parametrize("v, expected", [(0.3, 36.967), (0.5, 52.672), (1.0, 91.935)])
+@pytest.mark.filterwarnings("ignore:warning")
+def test_boussinesq_patch_sigma_x(v, expected):
+    """
+    Test the boussinesq patch method for horizontal stress against example from J Bowles (fig E11.8a)
+
+    Note that Bowles' results may have an error in them for the case of v = 1.0, where the result
+    calculated by this method is higher by about 6% than Bowles calculates.
+    """
+
+    height = 7.5
+    n_points = 10
+
+    z = np.linspace(0, height, n_points + 1)
+    z = (z[1:] + z[:-1]) / 2
+
+    delta_z = height / n_points
+
+    sigma_x = boussinesq_patch_sigma_x(
+        q=200,
+        x=4,
+        y=0,
+        z=z,
+        v=v,
+        x_patch=2,
+        y_patch=4,
+    )
+
+    actual = np.sum(sigma_x * delta_z)
+
+    assert isclose(expected, actual, rel_tol=0.05)
--- utilityscripts/geotech.py
+++ utilityscripts/geotech.py
@@ -2,7 +2,10 @@
 Contains some methods for working with geotech properties.
 """
 
+import warnings
 from math import exp, pi, radians, tan
+
+import numpy as np
 
 
 def q_ult(
@@ -108,3 +111,243 @@ def N_gamma_EC7(N_q, phi):
     """
 
     return 2.0 * (N_q - 1) * tan(phi)
+
+
+def boussinesq_point_sigma_z(Q, r, z):
+    """
+    Determine the vertical stresses due to a point load on the surface according to
+    Boussinesq's method.
+
+    :param Q: The point load.
+    :param r: The radial distance between the point load and the point at which
+        stresses are to be determined.
+    :param z: The depth between the surface and the point at which stresses are
+        required.
+    """
+
+    a = (3 * Q) / (2 * pi * z**2)
+    b = (1 / (1 + (r / z) ** 2)) ** (5 / 2)
+
+    return a * b
+
+
+def boussinesq_point_sigma_r(Q, r, z, v):
+    """
+    Determine the horizontal stresses due to a point load on the surface according to
+    Boussinesq's method. These stresses are in the radial direction away from the
+    point load.
+
+    Note that the method assumes an infinite field for these stresses to develop in.
+    For applications to retaining walls the wall itself cuts off this infinite field.
+    Some references suggest that the pressures on the wall may need to be multiplied
+    by 2x to account for this effect.
+
+    :param Q: The point load.
+    :param r: The radial distance between the point load and the point at which
+        stresses are to be determined.
+    :param z: The depth between the surface and the point at which stresses are
+        required.
+    :param v: Poisson's ratio for the soil. At v=0.5, there is no tangential stress.
+    """
+
+    a = Q / (2 * pi)
+
+    b = (3 * z * r**2) / ((r**2 + z**2) ** (5 / 2))
+    c = (1 - 2 * v) / (r**2 + z**2 + z * ((r**2 + z**2) ** (1 / 2)))
+
+    d = b - c
+
+    return a * d
+
+
+def boussinesq_point_sigma_theta(Q, r, z, v):
+    """
+    Determine the horizontal stresses due to a point load on the surface according to
+    Boussinesq's method. These stresses are in the tangential direction around the
+    point load.
+
+    Note that the method assumes an infinite field for these stresses to develop in.
+    For applications to retaining walls the wall itself cuts off this infinite field.
+    Some references suggest that the pressures on the wall may need to be multiplied
+    by 2x to account for this effect.
+
+    :param Q: The point load.
+    :param r: The radial distance between the point load and the point at which
+        stresses are to be determined.
+    :param z: The depth between the surface and the point at which stresses are
+        required.
+    :param v: Poisson's ratio for the soil. At v=0.5, there is no tangential stress.
+    """
+
+    a = Q / (2 * pi)
+    b = 1 - 2 * v
+    c = z / ((r**2 + z**2) ** (3 / 2))
+    d = 1 / (r**2 + z**2 + z * ((r**2 + z**2) ** (1 / 2)))
+
+    e = c - d
+
+    return a * b * e
+
+
+def boussinesq_point_tau_rz(Q, r, z):
+    """
+    Determine the vertical shear stresses due to a point load on the surface according
+    to Boussinesq's method. These shear stresses are directed radially outwards from
+    the point load towards the point of assessment.
+
+    Note that the method assumes an infinite field for these stresses to develop in.
+    For applications to retaining walls the wall itself cuts off this infinite field.
+    Some references suggest that the pressures on the wall may need to be multiplied
+    by 2x to account for this effect.
+
+    :param Q: The point load.
+    :param r: The radial distance between the point load and the point at which
+        stresses are to be determined.
+    :param z: The depth between the surface and the point at which stresses are
+        required.
+    """
+
+    a = (3 * Q) / (2 * pi)
+    b = (r * z**2) / ((r**2 + z**2) ** (5 / 2))
+
+    return a * b
+
+
+def boussinesq_point_sigma_x(Q, x, y, z, v):
+    """
+    Determine the horizontal stresses due to a point load on the surface according to
+    Boussinesq's method. These stresses are in the "x" direction where x and y are
+    defined as follows:
+
+    * x is perpendicular to a vertical plane through the soil (e.g. a retaining wall)
+    * y is parallel to the vertical plane.
+
+    Note 1: the method assumes an infinite field for these stresses to develop in.
+    For applications to retaining walls the wall itself cuts off this infinite field.
+    Some references suggest that the pressures on the wall may need to be multiplied
+    by 2x to account for this effect.
+
+    Note 2: this method ignores sigma_theta as per J Bowles 5ed fig 11-16.
+
+    :param Q: The point load.
+    :param x: the distance from the point load perpendicular to a vertical plane through
+       the soil (e.g. a retaining wall)
+    :param y: the distance between the point load and the point of interest on the
+        vertical plane.
+    :param z: The depth between the surface and the point at which stresses are
+        required.
+    :param v: Poisson's ratio for the soil. At v=0.5, there is no tangential stress.
+    """
+
+    r = (x**2 + y**2) ** (1 / 2)
+
+    sigma_r = boussinesq_point_sigma_r(Q=Q, r=r, z=z, v=v)
+
+    x_ratio = 1 if x == 0 and r == 0 else x / r
+    return sigma_r * x_ratio
+
+
+def boussinesq_patch_sigma_z(q, x, y, z, x_patch, y_patch, n_int_x=10, n_int_y=10):
+    """
+    Determine the vertical stresses due to a rectangular patch load on the surface
+    according to Boussinesq's method.
+    Calculated by integrating a number of finite points.
+
+    * x is perpendicular to a vertical plane through the soil (e.g. a retaining wall)
+    * y is parallel to the vertical plane.
+
+    Note 1: the method assumes an infinite field for these stresses to develop in.
+    For applications to retaining walls the wall itself cuts off this infinite field.
+    Some references suggest that the pressures on the wall may need to be multiplied
+    by 2x to account for this effect.
+
+    :param q: The applied pressure.
+    :param x: the distance from the centroid of the load patch to a vertical plane through
+       the soil (e.g. a retaining wall)
+    :param y: the distance from the centroid of the load patch and the point of interest
+        on the vertical plane.
+    :param z: The depth between the surface and the point at which stresses are
+        required.
+    :param x_patch: the width of the load patch in the x direction.
+    :param y_patch: the width of the load patch in the y direction.
+    :param n_int_x: The number of elements to break the load into in the z direction.
+    :param n_int_y: The number of elements to break the load into in the y direction.
+    """
+
+    if (x_patch / 2) > x:
+        warnings.warn("Load patch crosses over the plane of application")
+
+    Q_total = q * (x_patch * y_patch)
+    Q = Q_total / (n_int_y * n_int_x)
+
+    x_points = np.linspace(x - x_patch / 2, x + x_patch / 2, n_int_x + 1)
+    x_points = (x_points[1:] + x_points[:-1]) / 2
+
+    y_points = np.linspace(y - y_patch / 2, y + y_patch / 2, n_int_y + 1)
+    y_points = (y_points[1:] + y_points[:-1]) / 2
+
+    sigma_z = 0
+
+    for xi in x_points:
+        for yi in y_points:
+            r = (xi**2 + yi**2) ** (1 / 2)
+
+            sigma_z += boussinesq_point_sigma_z(Q=Q, r=r, z=z)
+
+    return sigma_z
+
+
+def boussinesq_patch_sigma_x(q, x, y, z, v, x_patch, y_patch, n_int_x=10, n_int_y=10):
+    """
+    Determine the horizontal stresses due to a rectangular patch load on the surface
+    according to Boussinesq's method. Stresses are calculated on a vertical plane
+    through the soil.
+    Calculated by integrating a number of finite points.
+
+    * x is perpendicular to a vertical plane through the soil (e.g. a retaining wall)
+    * y is parallel to the vertical plane.
+
+    Note 1: the method assumes an infinite field for these stresses to develop in.
+    For applications to retaining walls the wall itself cuts off this infinite field.
+    Some references suggest that the pressures on the wall may need to be multiplied
+    by 2x to account for this effect.
+
+    Note 2: this method ignores sigma_theta as per J Bowles 5ed fig 11-16.
+
+    Note 3: If the patch overlaps the point x, then some loads will cancel each other
+    out. This may result in incorrect pressure distributions (you may need to consider
+    splitting the footing in half to determine the pressure accurately).
+
+    :param q: The applied pressure.
+    :param x: the distance from the centroid of the load patch to a vertical plane through
+       the soil (e.g. a retaining wall)
+    :param y: the distance from the centroid of the load patch and the point of interest
+        on the vertical plane.
+    :param z: The depth between the surface and the point at which stresses are
+        required.
+    :param v: Poisson's ratio for the soil. At v=0.5, there is no tangential stress.
+    :param x_patch: the width of the load patch in the x direction.
+    :param y_patch: the width of the load patch in the y direction.
+    :param n_int_x: The number of elements to break the load into in the z direction.
+    :param n_int_y: The number of elements to break the load into in the y direction.
+    """
+
+    if (x_patch / 2) > x:
+        warnings.warn("Load patch crosses over the plane of application")
+
+    Q_total = q * (x_patch * y_patch)
+    Q = Q_total / (n_int_y * n_int_x)
+
+    x_points = np.linspace(x - x_patch / 2, x + x_patch / 2, n_int_x + 1)
+    x_points = (x_points[1:] + x_points[:-1]) / 2
+
+    y_points = np.linspace(y - y_patch / 2, y + y_patch / 2, n_int_y + 1)
+    y_points = (y_points[1:] + y_points[:-1]) / 2
+
+    sigma_x = 0
+
+    for xi in x_points:
+        for yi in y_points:
+            sigma_x += boussinesq_point_sigma_x(Q=Q, x=xi, y=yi, z=z, v=v)
+
+    return sigma_x
