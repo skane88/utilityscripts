ca90bc594d3f2b434b0b781a4559a9338b77fabc
Sean Kane <sean.kane@outlook.com.au>
2023-08-17 18:24:15 +1000

    add some methods to the steel design check.

--- utilityscripts/steel.py
+++ utilityscripts/steel.py
@@ -14,3 +14,66 @@ def alpha_m(*, M_m, M_2, M_3, M_4):
     """
 
     return 1.7 * M_m / (M_2**2 + M_3**2 + M_4**2) ** 0.5
+
+
+def alpha_v(*, d_p, t_w, f_y, s, f_yref=250.0):
+    """
+    Calculate the stiffened web shear buckling parameter alpha_v as per
+    AS4100 S5.11.5.2.
+
+    :param d_p: The depth of the web panel.
+    :param t_w: The thickness of the web.
+    :param f_y: The yield strength of the web panel.
+    :param s: The length of the web or spacing of vertical stiffeners that meet the
+        requirement of AS4100.
+    :param f_yref: The reference yield stress, nominally 250.
+    """
+
+    a1 = (82 / ((d_p / t_w) * (f_y / f_yref) ** 0.5)) ** 2
+
+    a2_param = 1.0 if (s / d_p) <= 1.0 else 0.75
+
+    a2 = (a2_param / ((s / d_p) ** 2)) + 1.0
+
+    return min(a1 * a2, 1.0)
+
+
+def alpha_d(*, alpha_v, d_p, s):
+    """
+    Calculate the stiffened web shear buckling parameter alpha_d as per
+    AS4100 S5.11.5.2.
+
+    Does not check for the presence of a stiffened end post.
+
+    :param alpha_v: The stiffened web shear buckling parameter alpha_v as per
+        AS4100 S5.11.5.2.
+    :param d_p: The depth of the web panel.
+    :param s: The length of the web or spacing of vertical stiffeners that meet the
+        requirement of AS4100.
+    """
+
+    return 1 + ((1 - alpha_v) / (1.15 * alpha_v * (1 + (s / d_p) ** 2) ** 0.5))
+
+
+def as_s5_15_3(*, gamma, a_w, alpha_v, v_star, v_u, d_p, s, phi=0.9):
+    """
+    Calculate the minimum area of a transverse shear stiffener as per AS4100 S5.15.3
+
+    :param gamma: The stiffener type factor.
+    :param a_w: The area of the web.
+    :param alpha_v: The stiffened web shear buckling parameter alpha_v as per
+        AS4100 S5.11.5.2.
+    :param v_star: The shear in the web.
+    :param v_u: The ultimate shear capacity of the web.
+    :param d_p: The depth of the web panel.
+    :param s: The length of the web or spacing of vertical stiffeners that meet the
+        requirement of AS4100.
+    :param phi: The capacity reduction factor
+    """
+
+    a_1 = 0.5 * gamma * a_w
+    a_2 = 1 - alpha_v
+    a_3 = v_star / (phi * v_u)
+    a_4 = (s / d_p) - ((s / d_p) ** 2 / (1 + (s / d_p) ** 2) ** 0.5)
+
+    return a_1 * a_2 * a_3 * a_4
