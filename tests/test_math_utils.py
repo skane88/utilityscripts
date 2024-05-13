7706ab8d8c002a3ed716c24847b1c3efc0bed602
Sean Kane <sean.kane@outlook.com.au>
2024-05-14 09:00:28 +1000

    added m_ceil method.

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -4,7 +4,7 @@ File to test the math utilities file
 
 import pytest
 
-from utilityscripts.math_utils import m_floor, m_round
+from utilityscripts.math_utils import m_ceil, m_floor, m_round
 
 
 @pytest.mark.parametrize(
@@ -45,3 +45,27 @@ def test_m_round(x, base, expected):
 )
 def test_m_floor(x, base, expected):
     assert m_floor(x, base) == expected
+
+
+@pytest.mark.parametrize(
+    "x, base, expected",
+    [
+        (100, 1, 100),
+        (-100, 1, -100),
+        (12.34, 0.5, 12.5),
+        (-12.34, 0.5, -12.0),
+        (12.34, -0.5, 12.5),
+        (-12.34, -0.5, -12.0),
+        (1.30, 0.05, 1.30),
+        (
+            0.1 * 3 * 10,
+            1,
+            4.0,
+        ),  # this is a tricky one - 0.1*3/10 != 3.0 in floating point.
+        (4.6, 0.2, 4.6),
+        (1.39, 0.05, 1.40),
+        (4.3, 0.25, 4.50),
+    ],
+)
+def test_m_ceil(x, base, expected):
+    assert m_ceil(x, base) == expected
--- utilityscripts/math_utils.py
+++ utilityscripts/math_utils.py
@@ -21,6 +21,7 @@ def num_digits(number, max_digits=100):
         digits = -int(math.floor(math.log10(number))) + i
         if round(number, digits) == number:
             break
+
     return digits
 
 
@@ -76,6 +77,34 @@ def m_floor(x, base):
     return float(round(math.floor(x / base) * base, frac_digits))
 
 
+def m_ceil(x, base):
+    """
+    Floor a number to the nearest multiple.
+
+    Note: due to floating point math, it is necessary to
+    convert the numbers to Decimals first. This
+    will incur a performance overhead so only use this
+    method if there is no better way to do this function.
+
+    :param x: the number to round.
+    :param base: the multiple to round to.
+    """
+
+    x = Decimal(str(x))
+    base = Decimal(str(base))
+    base = abs(base)
+
+    # next need to find the number of digits in the base.
+    # this is because sometimes round(x / base) * base
+    # returns a floating point number slightly above or below
+    # the real result. It is therefore necessary to round
+    # the resulting value to the number of digits of
+    # base.
+    frac_digits = num_digits(base)
+
+    return float(round(math.ceil(x / base) * base, frac_digits))
+
+
 def round_significant(x, s: int = 3):
     """
     Round a number to a specified significant figure.
@@ -108,18 +137,3 @@ def round_significant(x, s: int = 3):
 
     # drop it back down to the original power of 10 & return
     return round(floated) * sig10pow * xsign
-
-
-if __name__ == "__main__":
-    round_pairs = [(11.1212341234, 0.25), (-12.34, 0.5), (0.0623413, 0.1)]
-
-    print()
-    print("Testing round_nearest")
-    print()
-    for x, a in round_pairs:
-        print(f"x: {x}, a: {a}, round_nearest={m_round(x, a)}")
-
-    print()
-    print("Done")
-    print("=" * 10)
-    print()
