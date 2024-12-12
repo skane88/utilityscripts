9f83d445b6d436554b7a34da247dcd52c8546ead
Sean Kane <sean.kane@outlook.com.au>
2024-12-12 11:12:44 +1000

    add AS1170.1 roof load calculator.

--- utilityscripts/loads.py
+++ utilityscripts/loads.py
@@ -2,7 +2,14 @@
 Utilities for working with loads
 """
 
+from enum import StrEnum
 from math import pi
+
+
+class RoofType(StrEnum):
+    ACCESSIBLE = "accessible"
+    ACCESSIBLEFROMGROUND = "accessible from ground"
+    OTHER = "other"
 
 
 def pipe_load(*, pipe_density, content_density, outer_diameter, inner_diameter):
@@ -30,3 +37,42 @@ def pipe_load(*, pipe_density, content_density, outer_diameter, inner_diameter):
         "content_weight": content_weight,
         "full_weight": full_weight,
     }
+
+
+def roof_load_as1170_1(
+    roof_type: RoofType | str, area: float | None
+) -> tuple[float, float]:
+    """
+    Calculate roof load as per AS1170.1 S3.5
+
+    Notes
+    -----
+    Ignores the distinction between structure and cladding
+    that AS1170 allows for OTHER type roofs.
+
+    Parameters
+    ----------
+    roof_type : RoofType | str
+        What sort of roof is it?
+    area : float | None
+        The tributary area.
+
+    Returns
+    -------
+    tuple[float, float]
+
+    The loads to apply as (pressure in kPa, point load in kN)
+    """
+
+    if roof_type == RoofType.ACCESSIBLE:
+        return 1.5, 1.8
+
+    if roof_type == RoofType.ACCESSIBLEFROMGROUND:
+        return 1.0, 1.8
+
+    if roof_type != RoofType.OTHER:
+        raise ValueError(
+            "Roof type should be a valid RoofType Enum or equivalent string."
+        )
+
+    return max(0.25, 1.8 / area + 0.12), 1.4
