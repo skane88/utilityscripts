b89ccbb770ea3f9f49e542ec629727f9cae4a1a9
Sean Kane <sean.kane@outlook.com.au>
2025-01-17 16:40:13 +1000

    update documentation for multi-interp.

--- utilityscripts/multiinterp.py
+++ utilityscripts/multiinterp.py
@@ -3,35 +3,43 @@ This file contains some interpolation functions.
 """
 
 import timeit
-from typing import List, Union
 
 import numpy as np
 
 
 def multi_interp(
     *,
-    x: Union[float, List[float], np.ndarray],
-    xp: Union[List[float], np.ndarray],
-    fp: Union[List[float], List[List[float]], np.ndarray],
+    x: float | list[float] | np.ndarray,
+    xp: list[float] | np.ndarray,
+    fp: list[float] | list[list[float]] | np.ndarray,
     left: float | None = None,
     right: float | None = None,
-) -> Union[float, np.ndarray]:
+) -> float | np.ndarray:
     """
     Implements an equivalent of np.interp that will interpolate through multiple
     sets of y points.
 
-    NOTE: this is NOT 2D interpolation. It is simply repeated 1d interpolation of the
+    Notes
+    -----
+    This is NOT 2D interpolation. It is simply repeated 1d interpolation of the
     same x interpolation through multiple rows of y values. If fp has a single row, this
     is equivalent to calling the numpy function np.interp.
 
-    :param x: The x points to interpolate.
-    :param xp: The original x data points. Must be sorted. Raises a value error if not
+    Parameters
+    ----------
+    x : Union[float, List[float], np.ndarray]
+        The x points to interpolate.
+    xp : Union[List[float], np.ndarray]
+        The original x data points. Must be sorted. Raises a value error if not
         sorted.
-    :param fp: The original y data points to interpolate.
-    :param left: An optional fill value to use if a value in x is less than the values
+    fp : Union[List[float], List[List[float]], np.ndarray]
+        The original y data points to interpolate.
+    left : float | None
+        An optional fill value to use if a value in x is less than the values
         in xp (and thus cannot be interpolated). If None, the value of fp at min(xp)
         will be used.
-    :param right: An optional fill value to use if a value in x is greater than the
+    right : float | None
+        An optional fill value to use if a value in x is greater than the
         values in xp (and thus cannot be interpolated). If None, the value of fp at
         max(xp) will be used.
     """
