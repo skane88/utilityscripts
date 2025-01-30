e2b20ebf6dda1ba790c3da899bc245f4a2f29ee5
Sean Kane <sean.kane@outlook.com.au>
2025-01-30 16:01:47 +1000

    Basic implementation of Result object.

--- utilityscripts/result.py
+++ utilityscripts/result.py
@@ -2,6 +2,76 @@
 A file to store results along with some reporting information.
 """
 
+from typing import Any
+
 
 class Result:
-    pass
+    """
+    Class to store results along with metadata.
+    """
+
+    def __init__(
+        self,
+        result: Any,
+        eqn: dict[str, Any] | None = None,
+        inputs: dict[str, Any] | None = None,
+        metadata: dict[str, Any] | None = None,
+    ):
+        self._result = result
+        self._eqn = eqn
+        self._inputs = inputs
+        self._metadata = metadata
+
+    @property
+    def result(self):
+        return self._result
+
+    @property
+    def eqn(self):
+        return self._eqn
+
+    @property
+    def inputs(self):
+        return self._inputs
+
+    @property
+    def metadata(self):
+        return self._metadata
+
+    def __str__(self):
+        if isinstance(self._result, str):
+            return self._result
+        return str(self._result)
+
+    def __bool__(self):
+        if isinstance(self._result, bool):
+            return self._result
+        return bool(self._result)
+
+    def __int__(self):
+        if isinstance(self._result, int):
+            return self._result
+        return int(self._result)
+
+    def __float__(self):
+        if isinstance(self._result, float):
+            return self._result
+        return float(self._result)
+
+    def __bytes__(self):
+        if isinstance(self._result, bytes):
+            return self._result
+        return bytes(self._result)
+
+    def __complex__(self):
+        if isinstance(self._result, complex):
+            return self._result
+        return complex(self._result)
+
+    def __repr__(self):
+        return (
+            f"Result(result={self._result}, "
+            + f"eqn={self._eqn}, "
+            + f"inputs={None if self._inputs is None else f'{len(self._inputs)} inputs'}, "
+            + f"metadata={None if self._metadata is None else f'{len(self._metadata)} items'})"
+        )
