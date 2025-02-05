a3ff95e54a898346d50220045915a668cab76832
Sean Kane <sean.kane@outlook.com.au>
2025-02-05 17:55:37 +1000

    add a report to a result object.

--- utilityscripts/result.py
+++ utilityscripts/result.py
@@ -30,11 +30,13 @@ class Result:
     def __init__(
         self,
         result: Any,
+        title: str | None = None,
         eqn: dict[str, Any] | None = None,
         inputs: dict[str, Any] | None = None,
         metadata: dict[str, Any] | None = None,
     ):
         self._result = result
+        self._title = title
         self._eqn = eqn
         self._inputs = inputs
         self._metadata = metadata
@@ -46,6 +48,14 @@ class Result:
         """
 
         return self._result
+
+    @property
+    def title(self) -> str | None:
+        """
+        The title of the Result object.
+        """
+
+        return self._title
 
     @property
     def eqn(self) -> dict[str, Any] | None:
@@ -121,6 +131,27 @@ class Result:
         raise NotImplementedError(
             f"{self.result!r} does not support the __invert__ operation."
         )
+
+    @property
+    def report(self) -> str:
+        """
+        A short report on the result.
+        """
+
+        result_str = self.eqn + "\n\n" if self.eqn is not None else ""
+
+        if self._inputs is not None:
+            for i in self._inputs:
+                result_str += f"{i}: {self._inputs[i]}\n"
+
+        result_str = result_str + "\n" if result_str != "" else ""
+
+        if self.title is None:
+            result_str = result_str + self.result
+        else:
+            result_str = result_str + self.title + "=" + self.result
+
+        return result_str
 
     def __repr__(self):
         return (
