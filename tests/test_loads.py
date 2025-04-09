18741051b90c009a9cd1a07d8a5c7000e5694cdd
Sean Kane <sean.kane@outlook.com.au>
2025-04-09 10:55:11 +1000

    add functions to calculate crane dynamic loads.

---
+++ tests/test_loads.py
@@ -0,0 +1,79 @@
+"""
+Test functions in loads.py
+"""
+
+from math import isclose
+
+import pytest
+
+from utilityscripts.loads.loads import (
+    DriveClass,
+    HoistClass,
+    get_beta_2,
+    get_hoist_class,
+    get_phi_2_min,
+)
+
+
+@pytest.mark.parametrize(
+    "delta, expected",
+    [
+        (0.000, HoistClass.HC4),
+        (0.100, HoistClass.HC4),
+        (0.149, HoistClass.HC4),
+        (0.150, HoistClass.HC3),
+        (0.200, HoistClass.HC3),
+        (0.299, HoistClass.HC3),
+        (0.300, HoistClass.HC2),
+        (0.500, HoistClass.HC2),
+        (0.799, HoistClass.HC2),
+        (0.800, HoistClass.HC1),
+        (100.000, HoistClass.HC1),
+    ],
+)
+def test_get_hoist_class(delta: float, expected: HoistClass):
+    assert get_hoist_class(delta=delta) == expected
+
+
+@pytest.mark.parametrize(
+    "hoist_class, expected",
+    [
+        (HoistClass.HC1, 0.17),
+        (HoistClass.HC2, 0.34),
+        (HoistClass.HC3, 0.51),
+        (HoistClass.HC4, 0.68),
+    ],
+)
+def test_beta_2(hoist_class: HoistClass, expected: float):
+    assert isclose(get_beta_2(hoist_class=hoist_class), expected)
+
+
+@pytest.mark.parametrize(
+    "hoist_class, drive_class, expected",
+    [
+        (HoistClass.HC1, DriveClass.HD1, 1.05),
+        (HoistClass.HC1, DriveClass.HD2, 1.05),
+        (HoistClass.HC1, DriveClass.HD3, 1.05),
+        (HoistClass.HC1, DriveClass.HD4, 1.05),
+        (HoistClass.HC1, DriveClass.HD5, 1.05),
+        (HoistClass.HC2, DriveClass.HD1, 1.10),
+        (HoistClass.HC2, DriveClass.HD2, 1.10),
+        (HoistClass.HC2, DriveClass.HD3, 1.05),
+        (HoistClass.HC2, DriveClass.HD4, 1.10),
+        (HoistClass.HC2, DriveClass.HD5, 1.05),
+        (HoistClass.HC3, DriveClass.HD1, 1.15),
+        (HoistClass.HC3, DriveClass.HD2, 1.15),
+        (HoistClass.HC3, DriveClass.HD3, 1.05),
+        (HoistClass.HC3, DriveClass.HD4, 1.15),
+        (HoistClass.HC3, DriveClass.HD5, 1.05),
+        (HoistClass.HC4, DriveClass.HD1, 1.20),
+        (HoistClass.HC4, DriveClass.HD2, 1.20),
+        (HoistClass.HC4, DriveClass.HD3, 1.05),
+        (HoistClass.HC4, DriveClass.HD4, 1.20),
+        (HoistClass.HC4, DriveClass.HD5, 1.05),
+    ],
+)
+def test_phi_2_min(hoist_class: HoistClass, drive_class: DriveClass, expected: float):
+    assert isclose(
+        get_phi_2_min(hoist_class=hoist_class, drive_class=drive_class), expected
+    )
--- utilityscripts/loads/loads.py
+++ utilityscripts/loads/loads.py
@@ -3,7 +3,15 @@ Utilities for working with loads
 """
 
 from enum import StrEnum
+from functools import lru_cache
 from math import pi
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import polars as pl
+
+from utilityscripts.plotting import AGILITUS_COLORS
 
 
 class RoofType(StrEnum):
@@ -76,3 +84,142 @@ def roof_load_as1170_1(
         )
 
     return max(0.25, 1.8 / area + 0.12), 1.4
+
+
+_DATA_PATH = Path(Path(__file__).parent)
+CRANE_DATA_FILE = "crane_data.xlsx"
+
+
+class HoistClass(StrEnum):
+    HC1 = "HC1"
+    HC2 = "HC2"
+    HC3 = "HC3"
+    HC4 = "HC4"
+
+
+class DriveClass(StrEnum):
+    HD1 = "HD1"
+    HD2 = "HD2"
+    HD3 = "HD3"
+    HD4 = "HD4"
+    HD5 = "HD5"
+
+
+@lru_cache(maxsize=None)
+def _get_beta_2_data() -> pl.DataFrame:
+    return pl.read_excel(_DATA_PATH / Path(CRANE_DATA_FILE), sheet_name="beta_2")
+
+
+@lru_cache(maxsize=None)
+def _get_phi_2_data() -> pl.DataFrame:
+    return pl.read_excel(_DATA_PATH / Path(CRANE_DATA_FILE), sheet_name="phi_2_min")
+
+
+def get_hoist_class(*, delta: float) -> HoistClass:
+    """
+    Get the hoist class. The hoist class is assigned based on the elastic stiffness
+    of the crane. This is approximated by the characteristic deflection, delta.
+
+    Parameters
+    ----------
+    delta : float
+        The characteristic deflection.
+
+    Returns
+    -------
+    HoistClass
+        The hoist class.
+    """
+
+    beta_2_data = _get_beta_2_data()
+    beta_2_data = beta_2_data.filter(pl.col("delta_max") > delta)
+    beta_2_data = beta_2_data.filter(pl.col("delta_min") <= delta)
+
+    return HoistClass(beta_2_data["hoist_class"][0])
+
+
+def get_beta_2(*, hoist_class: HoistClass):
+    """
+    Get the value of beta_2 for a given hoisting class.
+
+    Parameters
+    ----------
+    hoist_class : HoistClass
+        The hoist class.
+
+    Returns
+    -------
+    float
+        The value of beta_2.
+    """
+
+    beta_2_data = _get_beta_2_data()
+    beta_2_data = beta_2_data.filter(pl.col("hoist_class") == hoist_class)
+    return beta_2_data["beta_2"][0]
+
+
+def get_phi_2_min(*, hoist_class: HoistClass, drive_class: DriveClass):
+    """
+    Get the minimum value of phi_2 for a given hoist class and drive class.
+
+    Parameters
+    ----------
+    hoist_class : HoistClass
+        The hoist class.
+    drive_class : DriveClass
+        The drive class.
+
+    Returns
+    -------
+    float
+        The minimum value of phi_2.
+    """
+
+    phi_2_data = _get_phi_2_data()
+    phi_2_data = phi_2_data.filter(pl.col("hoist_class") == hoist_class)
+    phi_2_data = phi_2_data.filter(pl.col("drive_class") == drive_class)
+    return phi_2_data["phi_2_min"][0]
+
+
+def phi_2(*, v_h: float, hoist_class: HoistClass, drive_class: DriveClass):
+    """
+    Calculate the dynamic hoisting factor, phi_2, as per S6.1.2.1 of AS5221.1-2021.
+
+    Parameters
+    ----------
+    v_h : float
+        The hoist speed.
+    hoist_class : HoistClass
+        The hoist class.
+    drive_class : DriveClass
+        The drive class.
+
+    Returns
+    -------
+    float
+        The dynamic hoisting factor.
+    """
+
+    beta_2 = get_beta_2(hoist_class=hoist_class)
+    phi_2_min = get_phi_2_min(hoist_class=hoist_class, drive_class=drive_class)
+
+    return phi_2_min + beta_2 * v_h
+
+
+def plot_phi_2(drive_class: DriveClass):
+    v_h = np.linspace(0, 2, 100)
+
+    ax = plt.gca()
+    ax.set_prop_cycle(AGILITUS_COLORS)
+
+    for hoist_class in HoistClass:
+        phi_2_values = [
+            phi_2(v_h=v, hoist_class=hoist_class, drive_class=drive_class) for v in v_h
+        ]
+        plt.plot(v_h, phi_2_values, label=hoist_class)
+
+    plt.xlabel("Hoist speed $v_h$ (m/s)")
+    plt.ylabel(r"Dynamic hoisting factor, $\phi_2$")
+    plt.title(f"Dynamic hoisting factor for drive class {drive_class}")
+    plt.legend()
+    plt.show()
