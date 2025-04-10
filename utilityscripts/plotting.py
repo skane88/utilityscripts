e6e461a136c3fcee0997a705eb1de9344f4ce153
Sean Kane <sean.kane@outlook.com.au>
2025-04-11 08:54:10 +1000

    update colormaps

--- utilityscripts/plotting.py
+++ utilityscripts/plotting.py
@@ -14,3 +14,16 @@ AGILITUS_COLORMAP = LinearSegmentedColormap.from_list(
 AGILITUS_BLUE_COLORMAP = LinearSegmentedColormap.from_list(
     "AGILITUS_BLUE", ["#0063ff", "#ffffff"]
 )
+
+PERSONAL_COLORS = cycler(
+    color=["#333333", "#CE190D", "#1B365D", "#707070", "#A3D5FF", "#F2A71B", "#268F63"]
+)
+PERSONAL_COLORMAP = LinearSegmentedColormap.from_list(
+    "PERSONAL", ["#CE190D", "#707070", "#1B365D"]
+)
+PERSONAL_RED_COLORMAP = LinearSegmentedColormap.from_list(
+    "PERSONAL_RED", ["#CE190D", "#FFFFFF"]
+)
+PERSONAL_BLUE_COLORMAP = LinearSegmentedColormap.from_list(
+    "PERSONAL_BLUE", ["#1B365D", "#FFFFFF"]
+)
