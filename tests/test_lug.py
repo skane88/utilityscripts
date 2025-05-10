10253cd376a14eba1e1cd6f1f29f8ef1f1981f0d
Sean Kane <sean.kane@outlook.com.au>
2025-05-10 12:41:46 +1000

    add basic test of LugLoad initialisation.

--- tests/test_lug.py
+++ tests/test_lug.py
@@ -7,7 +7,7 @@ from math import isclose, radians
 
 import pytest
 
-from utilityscripts.lifting_lug.lifting_lug import Lug
+from utilityscripts.lifting_lug.lifting_lug import Lug, LugLoad
 from utilityscripts.steel.steel import steel_grades
 
 
@@ -49,6 +49,50 @@ def create_eccentric_lug():
         e_hole=-0.035,
         material=create_steel(),
     )
+
+
+def test_lugload():
+    swl = 1.0
+    dia_pin = 0.020
+    daf = 1.2
+    uls = 1.0
+    hole_offset = 0.05
+    in_plane_angle_limits = (-radians(30), radians(30))
+    out_of_plane_angle_limits = (-radians(30), radians(30))
+    out_of_plane_allowance = 0.04
+    daf_out_of_plane = 1.0
+    uls_out_of_plane = 1.0
+    out_of_plane_offset = 0.020
+
+    ll = LugLoad(
+        swl=swl,
+        dia_pin=dia_pin,
+        dynamic_factor=daf,
+        uls_factor=uls,
+        hole_offset=hole_offset,
+        in_plane_angle_limits=in_plane_angle_limits,
+        out_of_plane_angle_limits=out_of_plane_angle_limits,
+        out_of_plane_allowance=out_of_plane_allowance,
+        daf_out_of_plane=daf_out_of_plane,
+        uls_out_of_plane=uls_out_of_plane,
+        out_of_plane_offset=out_of_plane_offset,
+        use_radians=True,
+    )
+
+    assert ll
+    assert ll.swl == swl
+    assert ll.dia_pin == dia_pin
+    assert ll.daf == daf
+    assert ll.uls == uls
+    assert ll.hole_offset == hole_offset
+    assert ll.min_in_plane_angle == in_plane_angle_limits[0]
+    assert ll.max_in_plane_angle == in_plane_angle_limits[1]
+    assert ll.min_out_of_plane_angle == out_of_plane_angle_limits[0]
+    assert ll.max_out_of_plane_angle == out_of_plane_angle_limits[1]
+    assert ll.out_of_plane_allowance == out_of_plane_allowance
+    assert ll.daf_out_of_plane == daf_out_of_plane
+    assert ll.uls_out_of_plane == uls_out_of_plane
+    assert ll.out_of_plane_offset == out_of_plane_offset
 
 
 @pytest.mark.parametrize(
