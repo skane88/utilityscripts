3aa5685ea1a4fecbeee660b32567135dc337ff9b
Sean Kane <sean.kane@outlook.com.au>
2025-05-30 06:23:38 +1000

    as3600 minor test update.

--- tests/test_as3600.py
+++ tests/test_as3600.py
@@ -23,6 +23,19 @@ def test_concrete_init():
     assert isclose(concrete.alpha_2, 0.82, rel_tol=1e-3)
     assert isclose(concrete.gamma, 0.92, rel_tol=1e-3)
 
+    concrete = Concrete(20)
+    assert concrete.f_c == 20  # noqa: PLR2004
+    assert concrete.density == 2400  # noqa: PLR2004
+    assert concrete.epsilon_c == 0.003  # noqa: PLR2004
+    assert concrete.poisson_ratio == 0.2  # noqa: PLR2004
+    assert isclose(concrete.f_cm, 25, rel_tol=1e-3)
+    assert isclose(concrete.f_cmi, 22, rel_tol=1e-3)
+    assert isclose(concrete.f_ctf, 2.683, rel_tol=1e-3)
+    assert isclose(concrete.f_ct, 1.61, rel_tol=1e-3)
+    assert isclose(concrete.elastic_modulus, 23700, rel_tol=1e-3)
+    assert isclose(concrete.alpha_2, 0.82, rel_tol=1e-3)
+    assert isclose(concrete.gamma, 0.92, rel_tol=1e-3)
+
 
 @pytest.mark.parametrize("f_c", [20, 25, 32, 40, 50, 65])
 def test_f_ctf_s3_1_1_3(f_c):
