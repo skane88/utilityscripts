50c671597efc60a69b18fd5710f8b53ce82a93eb
Sean Kane <sean.kane@outlook.com.au>
2025-06-11 09:02:24 +1000

    as1170.2 update c_pe_l to use polars.pivot to simplify code.

--- utilityscripts/wind/as1170_2.py
+++ utilityscripts/wind/as1170_2.py
@@ -13,6 +13,7 @@ import numpy as np
 import polars as pl
 from scipy.interpolate import RegularGridInterpolator
 
+# TODO: remove multi_interp and use scipy interpolator instead
 from utilityscripts.multiinterp import multi_interp
 
 FILE_PATH = Path(__file__)
@@ -1018,9 +1019,9 @@ def c_pe_l(
     roof_pitch : float
         The pitch of the roof. In degrees.
     d : float
-        The distance of the point under consideration from the windward end of the building.
+        The depth of the building into the wind.
     b : float
-        The width of the building.
+        The width of the building across the wind.
     roof_type : RoofType
         The type of roof. Note that this is from the direction the wind is blowing.
     version : StandardVersion, default=StandardVersion.AS1170_2_2021
@@ -1036,31 +1037,17 @@ def c_pe_l(
 
     c_pe_data = STANDARD_DATA["cpe_t5_2b"].filter(pl.col("version") == version)
     c_pe_data = c_pe_data.filter(pl.col("hip_or_gable") == roof_type)
+    c_pe_data = c_pe_data["roof_pitch", "d_b_ratio", "c_pe"].pivot(
+        "roof_pitch", index="d_b_ratio"
+    )
 
-    angles = c_pe_data["roof_pitch"].unique()
-
-    if roof_pitch == angles.max():
-        roof_pitch = roof_pitch - 0.00001
-    if roof_pitch == angles.min():
-        roof_pitch = roof_pitch + 0.00001
-
-    angle_smaller = angles.filter(angles <= roof_pitch).max()
-    angle_larger = angles.filter(angles > roof_pitch).min()
-
-    c_pe_smaller = c_pe_data.filter(pl.col("roof_pitch") == angle_smaller)
-    c_pe_larger = c_pe_data.filter(pl.col("roof_pitch") == angle_larger)
-
-    x = np.asarray([angle_smaller, angle_larger])
-    y = c_pe_smaller["d_b_ratio"].to_numpy()
-    z_small = c_pe_smaller["c_pe"].to_numpy()
-    z_large = c_pe_larger["c_pe"].to_numpy()
-    z = np.vstack([z_small, z_large])
+    x = np.float64(c_pe_data.columns[1:])
+    y = c_pe_data.to_numpy()[:, :1].flatten()
+    z = c_pe_data.to_numpy()[:, 1:].T
 
     interp = RegularGridInterpolator((x, y), z, bounds_error=True)
 
-    d_b_ratio = d / b
-
-    return float(interp((roof_pitch, d_b_ratio)))
+    return float(interp((roof_pitch, d / b)))
 
 
 def c_pe_s(
