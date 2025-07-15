10e4d6c40d9324ff6594e564653129c8816ff5e8
Sean Kane <sean.kane@outlook.com.au>
2025-07-15 10:07:40 +1000

    move GREEK_CHAR_MAP to its own file.

---
+++ src/utilityscripts/reports/_greek_chars.py
@@ -0,0 +1,54 @@
+"""
+Effectively a data file mapping greek characters to text inputs.
+"""
+
+GREEK_CHAR_MAP = {
+    ("alpha", "\\alpha"): ("α", "\\alpha"),  # noqa: RUF001
+    ("beta", "\\beta"): ("β", "\\beta"),
+    ("gamma", "\\gamma"): ("γ", "\\gamma"),  # noqa: RUF001
+    ("delta", "\\delta"): ("δ", "\\delta"),
+    ("epsilon", "\\epsilon"): ("ε", "\\epsilon"),
+    ("zeta", "\\zeta"): ("ζ", "\\zeta"),
+    ("eta", "\\eta"): ("η", "\\eta"),
+    ("theta", "\\theta"): ("θ", "\\theta"),
+    ("iota", "\\iota"): ("ι", "\\iota"),  # noqa: RUF001
+    ("kappa", "\\kappa"): ("κ", "\\kappa"),
+    ("lambda", "\\lambda"): ("λ", "\\lambda"),
+    ("mu", "\\mu"): ("μ", "\\mu"),
+    ("nu", "\\nu"): ("ν", "\\nu"),  # noqa: RUF001
+    ("xi", "\\xi"): ("ξ", "\\xi"),
+    ("omicron", "\\omicron"): ("ο", "\\omicron"),  # noqa: RUF001
+    ("pi", "\\pi"): ("π", "\\pi"),
+    ("rho", "\\rho"): ("ρ", "\\rho"),  # noqa: RUF001
+    ("sigma", "\\sigma"): ("σ", "\\sigma"),  # noqa: RUF001
+    ("tau", "\\tau"): ("τ", "\\tau"),
+    ("upsilon", "\\upsilon"): ("υ", "\\upsilon"),  # noqa: RUF001
+    ("phi", "\\phi"): ("φ", "\\phi"),
+    ("chi", "\\chi"): ("χ", "\\chi"),
+    ("psi", "\\psi"): ("ψ", "\\psi"),
+    ("omega", "\\omega"): ("ω", "\\omega"),
+    ("Alpha", "ALPHA", "\\Alpha"): ("Α", "\\Alpha"),  # noqa: RUF001
+    ("Beta", "BETA", "\\Beta"): ("Β", "\\Beta"),  # noqa: RUF001
+    ("Gamma", "GAMMA", "\\Gamma"): ("Γ", "\\Gamma"),
+    ("Delta", "DELTA", "\\Delta"): ("Δ", "\\Delta"),
+    ("Epsilon", "EPSILON", "\\Epsilon"): ("Ε", "\\Epsilon"),  # noqa: RUF001
+    ("Zeta", "ZETA", "\\Zeta"): ("Ζ", "\\Zeta"),  # noqa: RUF001
+    ("Eta", "ETA", "\\Eta"): ("Η", "\\Eta"),  # noqa: RUF001
+    ("Theta", "THETA", "\\Theta"): ("Θ", "\\Theta"),
+    ("Iota", "IOTA", "\\Iota"): ("Ι", "\\Iota"),  # noqa: RUF001
+    ("Kappa", "KAPPA", "\\Kappa"): ("Κ", "\\Kappa"),  # noqa: RUF001
+    ("Lambda", "LAMBDA", "\\Lambda"): ("Λ", "\\Lambda"),
+    ("Mu", "MU", "\\Mu"): ("Μ", "\\Mu"),  # noqa: RUF001
+    ("Nu", "NU", "\\Nu"): ("Ν", "\\Nu"),  # noqa: RUF001
+    ("Xi", "XI", "\\Xi"): ("Ξ", "\\Xi"),
+    ("Omicron", "OMICRON", "\\Omicron"): ("Ο", "\\Omicron"),  # noqa: RUF001
+    ("Pi", "PI", "\\Pi"): ("Π", "\\Pi"),
+    ("Rho", "RHO", "\\Rho"): ("Ρ", "\\Rho"),  # noqa: RUF001
+    ("Sigma", "SIGMA", "\\Sigma"): ("Σ", "\\Sigma"),
+    ("Tau", "TAU", "\\Tau"): ("Τ", "\\Tau"),  # noqa: RUF001
+    ("Upsilon", "UPSIGMA", "\\Upsilon"): ("Υ", "\\Upsilon"),  # noqa: RUF001
+    ("Phi", "PHI", "\\Phi"): ("Φ", "\\Phi"),
+    ("Chi", "CHI", "\\Chi"): ("Χ", "\\Chi"),  # noqa: RUF001
+    ("Psi", "PSI", "\\Psi"): ("Ψ", "\\Psi"),
+    ("Omega", "OMEGA", "\\Omega"): ("Ω", "\\Omega"),
+}
