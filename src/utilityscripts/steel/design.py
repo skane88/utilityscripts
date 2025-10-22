9f60bd5d17708e436e4204e5fe67a27d2d16b996
skane88 <sean.kane@outlook.com.au>
2025-10-22 14:21:33 +1000

    update default formatting.

--- src/utilityscripts/steel/design.py
+++ src/utilityscripts/steel/design.py
@@ -925,7 +925,7 @@ class AS4100:
             symbol=("N_ty", "N_{ty}"),
             scale=self.unit_system.force_scale,
             units=self.unit_system.force,
-            fmt_string=".2f",
+            fmt_string=".4j",
         )
 
     def phi_n_ty(self, phi_steel: float = 0.9) -> Variable:
@@ -947,7 +947,7 @@ class AS4100:
             symbol=("φN_ty", "\\phi N_{ty}"),
             scale=self.unit_system.force_scale,
             units=self.unit_system.force,
-            fmt_string=".2f",
+            fmt_string=".4j",
         )
 
     def n_tu(self, fracture_modifier: float = 0.85) -> Variable:
@@ -968,7 +968,7 @@ class AS4100:
             self.section.area_net * self.section.f_u_min * fracture_modifier,
             scale=0.001,
             units="kN",
-            fmt_string=".2f",
+            fmt_string=".4j",
         )
 
     def phi_n_tu(
@@ -993,7 +993,7 @@ class AS4100:
             self.n_tu(fracture_modifier=fracture_modifier).value * phi_steel,
             scale=0.001,
             units="kN",
-            fmt_string=".2f",
+            fmt_string=".4j",
         )
 
 
