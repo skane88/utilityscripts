27d22936f19fd0eb763fd3e9ab533fa6ad22e982
Sean Kane <skane88@users.noreply.github.com>
2025-11-10 13:29:54 +1000

    Add concrete cone equations.

--- src/utilityscripts/concrete/as5216.py
+++ src/utilityscripts/concrete/as5216.py
@@ -143,3 +143,125 @@ def cracked(*, sigma_l, sigma_r: float = 3.0, f_ct: float = 0.0):
     sigma_total = sigma_l + sigma_r
     
     return Concrete.UNCRACKED if sigma_total <= f_ct else Concrete.CRACKED
+
+def s6_2_3_1_n_rkc(*, n_0_rkc, a_cn, a_0_cn, psi_s_n, psi_re_n, psi_ec_n, psi_m_n):
+    """
+    Calculate the concrete cone failure capacity.
+    
+    Parameters
+    ----------
+    n_0_rkc :
+        The reference characteristic strength.
+    a_cn :
+        The projected cone failure area.
+    a_0_cn :
+        The reference projected cone failure arwa
+    psi_s_n :
+        The edge parameter to account for the effects of edges.
+    psi_re_n :
+        The shell spalling parameter.
+    psi_ec_n :
+        The ecccentricity parameter.
+    psi_m_n :
+        The axial force parameter.
+    """
+    
+    return n_0_rkc * (a_cn / a_0_cn) * psi_s_n * psi_re_n * psi_ec_n * psi_m_n
+
+
+def s6_2_3_2_n_0_rkc(*, k_1, f_c, h_ef):
+    """
+    The concrete cone failure capcity of a single anchor in reference conditions.
+    
+    Parameters
+    ----------
+    k_1 :
+        The tensile loading factor. This varies based on anchor type and concrete type. See Appendix A or s6.2.3.2.
+    f_c :
+        The concrete characteristic strength. In MPa.
+    h_ef : 
+        Anchor embedment length.  
+    """
+    
+    return k_1 * (f_c)**0.5 * h_ef**1.5
+
+
+def s6_2_3_3_c_cr_n(*, h_ef):
+    """
+    Characteristic edge distance of a reference anchor
+    
+    Parameters
+    ----------
+    h_ef :
+        Embedment depth.
+    """
+    
+    return 1.5 * h_ef
+    
+def s6_2_3_3_s_cr_n(*, h_ef):
+    """
+    Characteristic spacing required to allow development of the full anchor capacity.
+    
+    Parameters
+    ----------
+    h_ef :
+        Embedment depth.
+    """
+    
+    return 2 * s6_2_3_3_c_cr_n(h_ef = h_ef)
+    
+def s6_2_3_3_a_0_cn(*, h_ef):
+    """
+    Reference projected area of a reference anchor
+    
+    Parameters
+    ----------
+    h_ef :
+        Embedment depth.
+    """
+    
+    return s6_2_3_3_s_cr_n(h_ef=h_ef)**2
+
+def s6_2_3_4_psi_s_n(*, c, c_cr_n):
+    """
+    Edge effect factor for the effects of an edge.
+    
+    Parameters
+    ----------
+    c :
+        The actual edge distance.
+    c_c_rn :
+        The characteristic edge distance of a reference anchor. 
+    """
+    
+    return min(0.7 + 0.3 * (c / c_c_rn), 1.0)
+
+def s6_2_3_5_psi_re_n(*, h_ef):
+    """
+    The shell spalling factor. This accounts for a potential plane of weakness introduced by layers of reinforcement.
+    
+    Notes
+    -----
+    - AS5216 includes requirements on reinforcement size and spacing. If these are met this factor may be taken as 1.0.
+    
+    Parameters
+    ----------
+    h_ef :
+        The embedment depth. In mm.
+    """
+    
+    return min(0.5 + h_ef / 200, 1.0)
+
+def s6_2_3_6_psi_ec_n(*, e_n, s_cr_n):
+    """
+    The eccentricity factor. This accounts for the effects of eccentricity which result in uneven loads on anchors.
+    
+    Parameters
+    ----------
+    e_n :
+        Eccentricity of the resultant tensile force on the anchor pattern.
+    s_cr_n : 
+        The spacing required to allow an anchor to achieve it's reference capacity.
+    """
+    
+    return min(1.0, 1 / (1 + 2 * e_n / s_cr_n))
